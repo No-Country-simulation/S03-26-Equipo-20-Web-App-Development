13c3e01b22fe2c09541006eae7c3c8abf6c95c23
mmolinero12 <mmolinero12@gmail.com>
2026-03-19 08:18:36 -0600

    pom.xml actualizado con JJWT. Se modificó application.properties y @SpringBootApplication para evitar el uso de Flyway y JPA

--- src/main/java/org/precioustestimonials/cms/CmsApplication.java
+++ src/main/java/org/precioustestimonials/cms/CmsApplication.java
@@ -2,12 +2,18 @@ package org.precioustestimonials.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
 public class CmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
+        System.out.println("🚀 ¡El CMS está vivo y DevTools activo!");
+        System.out.println("Woooowww !!!!!!");
+
 	}
 
 }
